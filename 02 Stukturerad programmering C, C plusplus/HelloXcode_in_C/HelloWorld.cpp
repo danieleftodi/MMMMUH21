1f8107f3826a280fd9774731c9a432e06d3949f2
DEft <daniel@eftodi.se>
2021-09-14 14:06:18 +0200

    Adding 1st examples in our new course: 02 Stukturerad programmering C, C plusplus

---
+++ 02 Stukturerad programmering C, C plusplus/HelloXcode_in_C/HelloWorld.cpp
@@ -0,0 +1,21 @@
+//
+//  HelloWorld.c
+//  HelloXcode_in_C
+//
+//  Created by Daniel Eftodi on 2021-09-14.
+//
+
+#include "HelloWorld.h"
+#include <iostream>
+
+int main(){
+    //Maknig the HelloWorld print example with the printf()-command ... 
+    printf("Hello World\n");
+    printf("\nHi my name is Daniel, an I'm a h4xx0rz\n");
+
+    //Making the HelloWorld print example with the std::cout-command ...
+    std::cout << "\nHello World from the cout thingie\n";
+
+    //Endnig with an extra new line
+    std::cout << "\n";
+}
