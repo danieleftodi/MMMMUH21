289eac7ce905ee6d9ace35a1b55e8b38931aec99
DEft <daniel@eftodi.se>
2021-09-15 10:51:47 +0200

    updating file info

--- 02 Stukturerad programmering C, C plusplus/01_Syntax_and_Operations_on_macOS/01_Syntax_and_Operations_on_macOS.h
+++ 02 Stukturerad programmering C, C plusplus/01_Syntax_and_Operations_on_macOS/01_Syntax_and_Operations_on_macOS.h
@@ -5,9 +5,9 @@
 //  Created by Daniel Eftodi on 2021-09-15.
 //
 
-#ifndef HelloWorld_h
-#define HelloWorld_h
+#ifndef Syntax_and_Operations_on_macOS_h
+#define Syntax_and_Operations_on_macOS_h
 
 #include <stdio.h>
 
-#endif /* HelloWorld_h */
+#endif /* Syntax_and_Operations_on_macOS_h */
