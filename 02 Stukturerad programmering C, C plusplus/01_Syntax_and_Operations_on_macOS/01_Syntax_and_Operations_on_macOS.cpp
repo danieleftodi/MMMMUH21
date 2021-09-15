841eb07c602581b9a443eac0789daa6868fcce52
DEft <daniel@eftodi.se>
2021-09-15 09:46:10 +0200

    Examples for 01_Syntax_and_Operations_on_macOS
    From Day 2

---
+++ 02 Stukturerad programmering C, C plusplus/01_Syntax_and_Operations_on_macOS/01_Syntax_and_Operations_on_macOS.cpp
@@ -0,0 +1,23 @@
+//
+//  HelloWorld.c
+//  HelloXcode_in_C
+//
+//  Created by Daniel Eftodi on 2021-09-14.
+//
+
+#include "01_Syntax_and_Operations_on_macOS.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Maknig the HelloWorld print example with the printf()-command ... 
+    printf("Hello Syntax\n");
+
+    //Making the HelloWorld print example with the std::cout-command ...
+    cout << "\nHello cout Syntax\n";
+
+    //Endnig with an extra new line
+    cout << "\n";
+    
+    return 0;
+}
---
+++ 02 Stukturerad programmering C, C plusplus/01_Syntax_and_Operations_on_macOS/01_Syntax_and_Operations_on_macOS.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorld.h
+//  HelloXcode_in_C
+//
+//  Created by Daniel Eftodi on 2021-09-14.
+//
+
+#ifndef HelloWorld_h
+#define HelloWorld_h
+
+#include <stdio.h>
+
+#endif /* HelloWorld_h */
