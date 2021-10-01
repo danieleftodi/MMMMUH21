fe049661ad0791ce121b9657cd5618d675654f99
DEft <daniel@eftodi.se>
2021-10-01 10:30:55 +0200

    Adding: 04_Friday_24_Sept_2021

---
+++ 02 Stukturerad programmering C, C plusplus/04_Friday_24_Sept_2021/04_Friday_24_Sept_2021.h
@@ -0,0 +1,23 @@
+//
+//  04_Friday_24_Sept_2021.h
+//  04_Friday_24_Sept_2021
+//
+//  Created by Daniel Eftodi on 2021-09-14.
+//
+
+#ifndef Friday_24_Sept_2021_h
+#define Friday_24_Sept_2021_h
+
+#include <stdio.h>
+
+void function_three_exercise_five_gameOne_guess_number (void);
+
+void function_two_argc_argv_main (int f_argc, char ** f_argv);
+
+void function_three_exercise_five_main_menu (void);
+
+void function_three_exercise_five_gameOne_guess_number (void);
+
+void function_three_exercise_five (void);
+
+#endif /* Friday_24_Sept_2021_h */
